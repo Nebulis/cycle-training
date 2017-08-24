8904a4080b0ac732af47b31bd0d09c9c04de0953
Nebulis <nebounet@gmail.com>
2017-08-24 14:38:17 +0800

    step3 solution

--- src/index.js
+++ src/index.js
@@ -4,31 +4,47 @@ import {makeHTTPDriver} from '@cycle/http'
 import xs from 'xstream'
 import delay from 'xstream/extra/delay'
 
-// TODO 1 Create Training component
-// TODO 2 Add sources and use props as value
-// TODO 3 Reactive Element => xs.of
-// TODO 4 Use sinks
-// TODO 5 Listen for DOM click on .favorite and dont forget startWith
-// TODO 6 Use props as stream
+function Training(sources) {
+  const props$ = sources.props
+  const favorite$ = sources.DOM
+    .select('.favorite')
+    .events('click')
+    .fold(acc => !acc)
 
+  const state$ = xs.combine(props$, favorite$)
+    .map( ([props, favorite]) => ({
+      ...props,
+      favorite,
+    }))
 
-// TODO 2 Add sources and use props as value
-// TODO 6 Use props as stream
-function displayTrainings(trainings, sources) {
-  const view = [];
-  // TODO 7 isolation
-  for (const t of trainings) {
-    view.push(div(`.training.card.border-info`, [
+  const vdom$ = state$.map(training =>
+    div(`.training.card.border-info`, [
       div('.card-header.text-center', [
-        h3('.training-title.card-title', t.title),
-        i('.fa.fa-heart.favorite'),
+        h3('.training-title.card-title', training.title),
+        training.favorite ? i('.fa.fa-heart.favorite') : i('.fa.fa-heart-o.favorite'),
       ]),
       div('.card-body', [
-        div('.training-description.card-text', t.description)
+        div('.training-description.card-text', training.description)
       ]),
     ]))
+
+  return {
+    DOM: vdom$,
   }
-  return div('#training-container', view);
+}
+
+
+function displayTrainings(trainings, sources) {
+  const view = [];
+  const { isolateSource, isolateSink} = sources.DOM;
+  for (const t of trainings) {
+    const id = `training-${t.id}`
+    const training = Training({ DOM: isolateSource(sources.DOM, id), props: xs.of(t) })
+    view.push(isolateSink(training.DOM, id))
+  }
+  return xs
+    .combine(...view)
+    .map(trainings => div('#training-container', trainings));
 }
 
 function main (sources) {
@@ -41,11 +57,10 @@ function main (sources) {
     .select('training')
     .flatten();
 
-  // TODO 2 Add sources
-  // TODO 3 Reactive Element => flatten
   const vdom$ = response$
     .map(res => res.body)
-    .map(displayTrainings)
+    .map(t => displayTrainings(t, sources))
+    .flatten()
 
   return {
     DOM: vdom$,
