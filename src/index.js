ef3d439dfa4f73010407d816462dab1daf8ba0ae
Nebulis <nebounet@gmail.com>
2017-08-24 12:01:00 +0800

    step2 todo

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import {run} from '@cycle/run'
 import {makeDOMDriver, div} from '@cycle/dom'
+import {makeHTTPDriver} from '@cycle/http'
 import xs from 'xstream'
+import delay from 'xstream/extra/delay'
 
-function main () {
+function main (sources) {
+  // TODO 1 create request to /training.json
+
+  // TODO 3 listen for HTTP response (don't forget flatten at the end)
+
+  // TODO 4 display the numbers of elements
+  // TODO 5 add delay
   const vdom$ = xs.of(div('Hello World'))
 
   return {
@@ -10,6 +18,7 @@ function main () {
   }
 }
 
+// TODO 2 create HTTP driver
 const drivers = {
   DOM: makeDOMDriver('#app'),
 }
